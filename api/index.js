d0757090cd9852ecf9dc64386b312759379d00b0
Tomessy <paulmessmer17@gmail.com>
2021-05-17 15:29:08 -0500

    .

--- api/index.js
+++ api/index.js
@@ -1,49 +1,21 @@
 const express = require("express");
-// const helmet = require("helmet");
 const morgan = require("morgan");
 const path = require("path");
-//const routes = require("./routes");
 
 const app = express();
-
-
-// app.use(helmet());
-
-// Setup morgan which gives us HTTP request logging.
-//morgan.token("all", ":status :method :url reqAuth :req[Authorization]");
 
 app.use(morgan("dev"));
 
 // Sets up request body JSON parsing
 app.use(express.json());
 
-// Add api routes.
-//app.use("/api", routes);
-
-
 // // Tells express about the build folder
 app.use(express.static(path.join(__dirname, "../client/build")));
-
-// // Tells express about the build folder
-// app.use(express.static(path.join(__dirname, "../client/projects/crypto/build")));
-
-// // Tells express about the build folder
-// app.use(express.static(path.join(__dirname, "../client/projects/webflix/build")));
 
 // react html
 app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
-
-// // react html
-// app.get("/crypto", (req, res) => {
-// 	res.sendFile(path.join(__dirname, "../client/projects/crypto/build/index.html"));
-// });
-
-// // react html
-// app.get("/webflix*", (req, res) => {
-// 	res.sendFile(path.join(__dirname, "../client/projects/webflix/build/index.html"));
-// });
 
 // Sends a 404 error if no end points match the end points.
 app.use((req, res) => {
